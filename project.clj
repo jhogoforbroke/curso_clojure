8edc67cd95dce8034f80711f5eee29e694286473
Jonatas Marinho <jmarinho.dev@gmail.com>
2020-03-20 21:48:58 -0300

    init commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject curso_clojure "0.1.0-SNAPSHOT"
+  :description "Curso Clojure - material de referencia para meus estudos"
+  :url "https://github.com/jhogoforbroke/curso_clojure"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :repl-options {:init-ns curso-clojure.core})
